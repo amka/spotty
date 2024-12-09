bc03c5df8e95bc37b8463ebecd5744edab37d3f3
Andrey M <meamka@ya.ru>
2024-12-09 16:31:22 +0300

    Add integration test of getCoordinates

--- test/spotty_test.dart
+++ test/spotty_test.dart
@@ -2,7 +2,13 @@ import 'package:spotty/spotty.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('get coordinates', () async {
+    final results = await getCoordinates("London");
+    expect(results, isNotNull);
+    expect(results, isNotEmpty);
+
+    expect(results!.first.countryCode, "GB");
+    expect(results.first.latitude, 51.50853);
+    expect(results.first.longitude, -0.12574);
   });
 }
